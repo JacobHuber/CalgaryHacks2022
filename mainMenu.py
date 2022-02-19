9f60d5896f67a4918484c210486e354e36a96f12
Hayden McNally <60626160+HaydenMcNally@users.noreply.github.com>
2022-02-19 14:38:08 -0700

    fixed main menu
    main menu

--- mainMenu.py
+++ mainMenu.py
@@ -5,13 +5,18 @@ class MainMenu:
 
 
 
-    def __init__(self):
+    def __init__(self, sr):
         self.tickables = []
         self.drawables = []
+        self.sr = sr
 
-
+    def create_buttons(self):
+        testButton = Button("Test", pygame.Rect(200, 200, 200, 100), (100, 150, 20), print)
+        self.tickables.append(testButton)
+        self.drawables.append(testButton)
 
     def Menu():
+        create_buttons(self)
 
-        
+
         
