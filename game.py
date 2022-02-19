46b0d384d872cb86e2c77caec1f0625e5845ac1c
JacobHuber <xjacobhuber@gmail.com>
2022-02-19 15:07:48 -0700

    Removed button

--- game.py
+++ game.py
@@ -9,18 +9,11 @@ class Game:
 
 		self.tickables = []
 		self.drawables = []
-		
-		self.create_buttons()
+
 		self.create_player()
-
-	def create_buttons(self):
-		testButton = Button("Test", pygame.Rect(200, 200, 200, 100), (100, 150, 20), print)
-
-		self.tickables.append(testButton)
-		self.drawables.append(testButton)
 
 	def create_player(self):
 		self.player = Player(self)
-		
+
 		self.tickables.append(self.player)
 		self.drawables.append(self.player)
