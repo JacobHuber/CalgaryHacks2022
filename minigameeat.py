9f7a2f9573961692b627fca49ff4b81f47913ef2
JacobHuber <xjacobhuber@gmail.com>
2022-02-20 01:53:15 -0700

    Bean Hell

--- minigameeat.py
+++ minigameeat.py
@@ -7,7 +7,7 @@ from math import *
 class MinigameEat(Minigame):
 	def __init__(self, player, surface, color):
 		Minigame.__init__(self, player, surface, color)
-		self.games = [ShoppingGame(self)]
+		self.games = [ShoppingGame(self), BulletHellGame(self)]
 
 
 
@@ -107,3 +107,73 @@ class ShoppingGame:
 			surface.blit(self.imgRamen, self.ramenRect)
 
 		surface.blit(self.imgShoppingCart, self.shoppingCartRect)
+
+
+class BulletHellGame:
+	def __init__(self, mg):
+		self.mg = mg
+		self.gain = 0
+		self.end = False
+
+		self.image = pygame.image.load("pictures/beans.png", "png")
+		self.imageRect = self.image.get_rect()
+
+		self.beans = []
+		self.beanTime = 0.1 * self.mg.player.game.sr.FPS
+
+		self.beginHell = 0.4 * self.mg.player.game.sr.FPS
+		self.hellTime = 0 
+		self.endHell = 6 * self.mg.player.game.sr.FPS
+
+	def setup(self):
+		self.beans = []
+		self.gain = 0
+		self.end = False
+		self.hellTime = 0
+
+	def spawn_bean(self):
+		side = choice((True, False))
+		x = 0
+		y = 0
+		if (side):
+			x = choice((-50, self.mg.width + 50))
+			y = randint(0, self.mg.height)
+		else:
+			x = randint(0, self.mg.width)
+			y = choice((-50, self.mg.height + 50))
+
+		gotoX = self.mg.width // 2
+		gotoY = self.mg.height // 2
+
+		low = 0.4
+		xs = (gotoX - x) / ((random() + low) * 2 * self.mg.player.game.sr.FPS)
+		ys = (gotoY - y) / ((random() + low) * 2 * self.mg.player.game.sr.FPS)
+
+		bean = {"x":x, "y":y, "xs":xs, "ys":ys}
+		self.beans.append(bean)
+
+	def tick(self):
+		if (self.hellTime > self.beginHell):
+			if (self.mg.player.time % self.beanTime == 0):
+				self.spawn_bean()
+
+			for bean in self.beans:
+				bean["x"] += bean["xs"]
+				bean["y"] += bean["ys"]
+
+				self.imageRect.center = (bean["x"], bean["y"])
+				if (self.imageRect.collidepoint(self.mg.get_mouse())):
+					self.gain = -10
+					self.end = True
+
+			if (self.hellTime > self.endHell):
+				self.gain = 20
+				self.end = True
+
+		self.hellTime += 1
+
+
+	def draw(self, surface, font):
+		for bean in self.beans:
+			self.imageRect.center = (bean["x"], bean["y"])
+			surface.blit(self.image, self.imageRect)
