0d94f016c656b3fd7cc6d099e305915f286cbae6
Hayden McNally <60626160+HaydenMcNally@users.noreply.github.com>
2022-02-20 03:08:09 -0700

    Info and Day
    fixed day and change Info

--- infoMenu.py
+++ infoMenu.py
@@ -21,7 +21,7 @@ class InfoMenu:
 
 
     def create_buttons(self):
-        backButton = Button("Back", pygame.Rect(self.sr.WIDTH-450, self.sr.HEIGHT-150, 400, 100), (100, 150, 20), self.changeMain)
+        backButton = Button("Back", pygame.Rect(self.sr.WIDTH//2-200, self.sr.HEIGHT-150, 400, 100), (100, 150, 20), self.changeMain)
     
         self.addButton(backButton)
 
@@ -30,5 +30,11 @@ class InfoMenu:
         self.drawables.append(title)
 
         fontbody = pygame.font.Font("font.ttf",58)
-        body = Text(self.sr,fontbody,"This is how you play the game you idiot",(255,255,255),self.sr.WIDTH/2,self.sr.HEIGHT/2-100,100,100)
+        body = Text(self.sr,fontbody,"Keep all you meter bars up by performing tasks.",(255,255,255),self.sr.WIDTH/2,self.sr.HEIGHT/2-100,100,100)
         self.drawables.append(body)
+
+        body2 = Text(self.sr,fontbody,"If a meter runs out it's Game Over",(255,255,255),self.sr.WIDTH/2,self.sr.HEIGHT/2,100,100)
+        self.drawables.append(body2)
+
+        body2 = Text(self.sr,fontbody,"Last as long as you can!",(255,255,255),self.sr.WIDTH/2,self.sr.HEIGHT/2+100,100,100)
+        self.drawables.append(body2)
